bb59337d03fb34b495dfdfba1c5c7c1a99b65189
Lucca Kaiser <luccakaiser@gmail.com>
2018-11-18 20:10:53 +0100

    bugfix

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,10 +1,10 @@
 class Cocktail < ApplicationRecord
+  mount_uploader :photo, PhotoUploader
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
 
   validates :name, presence: true, uniqueness: true
 
 
-  mount_uploader :photo, PhotoUploader
 
 end
